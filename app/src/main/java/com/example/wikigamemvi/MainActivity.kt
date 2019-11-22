2598d77b1ce1a9b62b617c6bbbcf2ad92dc1879f
AKJAW <akjaworski1@gmail.com>
2019-11-22 12:41:35 +0100

    connected ArticlesFragment to MainActivity

--- app/src/main/java/com/example/wikigamemvi/MainActivity.kt
+++ app/src/main/java/com/example/wikigamemvi/MainActivity.kt
@@ -15,59 +15,61 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.plusAssign
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), ActionObservable<GameAction> {
-    private var disposables: CompositeDisposable = CompositeDisposable()
+class MainActivity : AppCompatActivity() {
+//    private var disposables: CompositeDisposable = CompositeDisposable()
 
-    private lateinit var viewModel: GameViewModel
+//    private lateinit var viewModel: GameViewModel
 
-    override fun events(): Observable<GameAction> {
-//        return testButton.clicks().publish {
-//            Observable.merge(
-//                it.map { GameAction.LoadTargetArticleAction },
-//                it.map { GameAction.ShowToastAction("aaa") }
-//            )
-//        }
-        return testButton.clicks().map { GameAction.ShowToastAction("aaa") }
-    }
+//    override fun events(): Observable<GameAction> {
+////        return testButton.clicks().publish {
+////            Observable.merge(
+////                it.map { GameAction.LoadTargetArticleAction },
+////                it.map { GameAction.ShowToastAction("aaa") }
+////            )
+////        }
+//        return testButton.clicks().map { GameAction.ShowToastAction("aaa") }
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProviders.of(this)
+        setSupportActionBar(toolbar)
+
+        val viewModel = ViewModelProviders.of(this)
             .get(GameViewModel::class.java)
 
     }
 
-    override fun onResume() {
-        super.onResume()
+//    override fun onResume() {
+//        super.onResume()
+//
+//        disposables += viewModel.viewState.subscribe(::render)
+//        disposables += viewModel.viewEffects.subscribe(::trigger)
+//        disposables += events().subscribe(viewModel::process)
+//    }
 
-        disposables += viewModel.viewState.subscribe(::render)
-        disposables += viewModel.viewEffects.subscribe(::trigger)
-        disposables += events().subscribe(viewModel::process)
-    }
+//    private fun render(state: GameViewState) {
+//        if(state.isTargetArticleLoading){
+//            testTextview.text = "Loading"
+//        }
+//
+//        if(state.targetArticle != null){
+//            testTextview.text = state.targetArticle.name
+//        }
+//    }
 
-    private fun render(state: GameViewState) {
-        if(state.isTargetArticleLoading){
-            testTextview.text = "Loading"
-        }
+//    private fun trigger(effect: GameViewEffect) {
+//        when(effect){
+//            is GameViewEffect.SomeToastEffect -> {
+//                Toast.makeText(this@MainActivity, effect.text, Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
 
-        if(state.targetArticle != null){
-            testTextview.text = state.targetArticle.name
-        }
-    }
-
-    private fun trigger(effect: GameViewEffect) {
-        when(effect){
-            is GameViewEffect.SomeToastEffect -> {
-                Toast.makeText(this@MainActivity, effect.text, Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        disposables.clear()
-    }
+//    override fun onPause() {
+//        super.onPause()
+//
+//        disposables.clear()
+//    }
 }
--- app/src/main/java/com/example/wikigamemvi/feature/game/ArticlesFragment.kt
+++ app/src/main/java/com/example/wikigamemvi/feature/game/ArticlesFragment.kt
@@ -1,7 +1,25 @@
 package com.example.wikigamemvi.feature.game
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.wikigamemvi.R
+import io.reactivex.disposables.CompositeDisposable
 
 class ArticlesFragment: Fragment(){
+    private var disposables = CompositeDisposable()
 
+    private lateinit var viewModel: GameViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_articles, container, false).also {
+
+        }
+    }
 }
